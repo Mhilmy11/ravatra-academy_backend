e9e23d44aa806417d872f9a6beca3857db7d2ff7
Mhilmy11 <setiawantojr@gmail.com>
2025-09-26 14:07:13 +0700

    update login logic

--- routes/login.php
+++ routes/login.php
@@ -2,6 +2,12 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 require_once(__DIR__ . '/../config/db.php');
 require_once(__DIR__ . '/../config/config.php');
