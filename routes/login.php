81e6a18fcc7fc430307e5737faa452903abb8a52
Mhilmy11 <setiawantojr@gmail.com>
2025-09-26 14:00:53 +0700

    update

--- routes/login.php
+++ routes/login.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 require_once(__DIR__ . '/../config/db.php');
 require_once(__DIR__ . '/../config/config.php');
